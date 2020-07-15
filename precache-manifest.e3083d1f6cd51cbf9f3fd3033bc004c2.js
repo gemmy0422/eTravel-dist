40841e2f5f36fcd164e8351d9b819fd4ddab6814
gemmy0422 <gemmy0422@hotmail.com.tw>
2020-07-16 00:04:14 +0800

    adjust commit

---
+++ precache-manifest.e3083d1f6cd51cbf9f3fd3033bc004c2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c17b809ddec7aa7cac2",
+    "url": "/eTravel/css/app.f724768e.css"
+  },
+  {
+    "revision": "2be11bea88c12fae6dff8fbc9b6f0aab",
+    "url": "/eTravel/img/airplane.2be11bea.svg"
+  },
+  {
+    "revision": "08519b8922349e38a6ff924b8ca362cf",
+    "url": "/eTravel/img/bed.08519b89.svg"
+  },
+  {
+    "revision": "95afc0d5e9bfb3c90b0cadcf7e3b0ed3",
+    "url": "/eTravel/img/car.95afc0d5.svg"
+  },
+  {
+    "revision": "974397c07bd559c43c97e3982babbcea",
+    "url": "/eTravel/img/down-arrow.974397c0.svg"
+  },
+  {
+    "revision": "c162beff2f8295757a4c1dbd59c0ab65",
+    "url": "/eTravel/img/down-chevron.c162beff.svg"
+  },
+  {
+    "revision": "bd9972cedb25bae442a7b4d3ef030fea",
+    "url": "/eTravel/img/facebook.bd9972ce.svg"
+  },
+  {
+    "revision": "5693085da8f36783356512ffadb10237",
+    "url": "/eTravel/img/line.5693085d.svg"
+  },
+  {
+    "revision": "1a47c5981af9474dde7cd8a99cc17b3e",
+    "url": "/eTravel/img/telegram.1a47c598.svg"
+  },
+  {
+    "revision": "f4aa0592e76709bfb72fe8849b73f65c",
+    "url": "/eTravel/img/youtube.f4aa0592.svg"
+  },
+  {
+    "revision": "48ad85f088560aab16095beffc05cc48",
+    "url": "/eTravel/index.html"
+  },
+  {
+    "revision": "6c17b809ddec7aa7cac2",
+    "url": "/eTravel/js/app.547bb7f0.js"
+  },
+  {
+    "revision": "0b9fa33a1a869947c11b",
+    "url": "/eTravel/js/chunk-vendors.c882ab25.js"
+  },
+  {
+    "revision": "374f32a80f8c54b9dfaf331595da1d65",
+    "url": "/eTravel/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/eTravel/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/eTravel/precache-manifest.e3083d1f6cd51cbf9f3fd3033bc004c2.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "etravel"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
